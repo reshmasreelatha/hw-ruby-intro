9a63544de30a0767f3080e4f3541e4f0061025aa
EC2 Default User <ec2-user@ip-172-31-26-23.us-east-2.compute.internal>
2018-02-14 01:03:36 +0000

    Homework 1

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,71 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  sum_arr = 0
+    if arr.empty?
+      sum_arr = 0
+    else
+      sum_arr = arr.reduce(:+)
+    end
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  sum = 0
+  if arr.empty?
+      sum = 0
+  else
+  sum = arr.max(2).reduce(:+)
+  end
 end
 
-def sum_to_n? arr, n
-  # YOUR CODE HERE
+def sum_to_n? arr, n 
+  if arr.empty?
+    false
+  end
+  sum = arr.combination(2).find { |x,y| x+y ==n }
+    return sum!=nil
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, " + name
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  if !s.is_a?(String) || s.empty?
+    false
+  else
+    if s.downcase.start_with?('b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z')
+      true
+    else
+      false
+    end
+  end
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  if s.gsub(/[01]/,'') == '' && !s.empty? && s.to_i(2) % 4 == 0
+    true
+  else
+    false
+  end
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  attr_accessor :isbn, :price
+  
+  def initialize(isbn,price)
+    raise ArgumentError.new("ISBN is empty string") if isbn.empty?
+    @isbn = isbn
+    raise ArgumentError.new("Price less than or equal to 0") if price <= 0
+    @price = price
+  end
+  
+  def price_as_string
+    "$#{"%0.2f" % @price}"
+  end
+  
 end
